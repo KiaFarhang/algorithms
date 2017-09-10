6b815093d2cc51f6131036d17977af599caf3535
Kia Farhang <kfarhang0@gmail.com>
2017-09-09 20:32:01 -0500

    Removed a bunch of babel/flow stuff

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,138 @@ import { Permutation } from './Basics/Permutation';
 import { LinkedListStack } from './LinkedList/LinkedListStack';
 import { LinkedListQueue } from './LinkedList/LinkedListQueue';
 
-let string = 'a b c d e f g';
+import { SortObject } from './Sorting/SortObject';
+import { SelectionSort } from './Sorting/SelectionSort';
+import { InsertionSort } from './Sorting/InsertionSort';
+import { ShellSort } from './Sorting/ShellSort';
+import { MergeSort } from './Sorting/MergeSort';
 
-let perm = new Permutation();
 
-perm.permutate(3, string);
+
+
+
+
+// const arrayOf100b: SortObject<number>[] = [];
+
+// for (let i: number = 0; i < 10; i++) {
+// 	arrayOf100b.push(new SortObject(Math.random()));
+// }
+
+// MergeSort.merge(arrayOf100b, 0, 4, 9);
+// console.log(arrayOf100b);
+
+
+// const arrayOf100a: SortObject<number>[] = [];
+
+// for (let i: number = 0; i < 10000; i++) {
+// 	arrayOf100a.push(new SortObject(Math.random()));
+// }
+
+// console.log(ShellSort.sort(arrayOf100a));
+
+
+// console.time('a');
+// InsertionSort.sort(arrayOf100a);
+// console.timeEnd('a');
+// console.time('b');
+// ShellSort.sort(arrayOf100b);
+// console.timeEnd('b');
+
+// console.time('a');
+// let test = 0;
+// while(test < 10){
+// 	for(let j = 0; j < 10; j++){
+// 		for(let k = 0; k < 10; k++){
+// 			let a = 5+5;
+// 		}
+// 	}
+// test++;	
+// }
+
+// console.timeEnd('a');
+
+// console.time('b');
+// for(let i = 0; i < 10; i++){
+// 	for(let j = 0; j < 600; j++){
+// 		let a = 5+5;
+// 	}
+// }
+// console.timeEnd('b');
+
+// let test = 2;
+// while(test > 0){
+// 	console.log(test);
+// 	test = Math.floor(test/2);
+// }
+
+// console.log(test);
+
+
+
+
+// console.time('Selection sort: 100 items');
+// SelectionSort.sort(arrayOf100);
+// console.timeEnd('Selection sort: 100 items');
+
+// console.time('Insertion sort: 100 items');
+// InsertionSort.sort(arrayOf100);
+// console.timeEnd('Insertion sort: 100 items');
+
+// console.time('Shellsort: 100 items');
+// ShellSort.sort(arrayOf100);
+// console.timeEnd('Shellsort: 100 items');
+
+
+// const arrayOf500: SortObject<number>[] = [];
+
+// for(let i: number = 0; i < 500; i++){
+// 	arrayOf500.push(new SortObject(Math.random()));
+// }
+
+// console.time('Selection sort: 500 items');
+// SelectionSort.sort(arrayOf500);
+// console.timeEnd('Selection sort: 500 items');
+
+// console.time('Insertion sort: 500 items');
+// InsertionSort.sort(arrayOf500);
+// console.timeEnd('Insertion sort: 500 items');
+
+// console.time('Shellsort: 500 items');
+// ShellSort.sort(arrayOf500);
+// console.timeEnd('Shellsort: 500 items');
+
+// const arrayOf1000: SortObject<number>[] = [];
+
+// for(let i: number = 0; i < 1000; i++){
+// 	arrayOf1000.push(new SortObject(Math.random()));
+// }
+
+// console.time('Selection sort: 1000 items');
+// SelectionSort.sort(arrayOf1000);
+// console.timeEnd('Selection sort: 1000 items');
+
+// console.time('Insertion sort: 1000 items');
+// InsertionSort.sort(arrayOf1000);
+// console.timeEnd('Insertion sort: 1000 items');
+
+// console.time('Shellsort: 1000 items');
+// ShellSort.sort(arrayOf1000);
+// console.timeEnd('Shellsort: 1000 items');
+
+// const arrayOf10000: SortObject<number>[] = [];
+
+// for(let i: number = 0; i < 10000; i++){
+// 	arrayOf10000.push(new SortObject(Math.random()));
+// }
+
+// console.time('Selection sort: 10000 items');
+// SelectionSort.sort(arrayOf10000);
+// console.timeEnd('Selection sort: 10000 items');
+
+// console.time('Insertion sort: 10000 items');
+// InsertionSort.sort(arrayOf10000);
+// console.timeEnd('Insertion sort: 10000 items');
+
+// console.time('Shellsort: 10000 items');
+// ShellSort.sort(arrayOf10000);
+// console.timeEnd('Shellsort: 10000 items');
